e1073c6553dcf867fcfb31576806f79dc73c392c
Gabriel Chaperon <gabrielchaperonb@gmail.com>
2021-02-04 09:36:44 -0300

    1-13, vertical hist

--- chapter1/1-13.c
+++ chapter1/1-13.c
@@ -6,7 +6,7 @@
 
 main()
 {
-	int i, j, c, curr_nchars, state;
+	int i, j, c, curr_nchars, state, max_count;
 	int lens[NBINS];
 	for (i = 0; i < NBINS; ++i)
 		lens[i] = 0;
@@ -30,16 +30,34 @@ main()
 		}
 	}
 
-	for (i = 0; i < NBINS -1; ++i) {
-		printf("%3d ", i + 1);
+	/* Prepare a matrix to draw histogram */
+	max_count = 0;
+	for (i = 0; i < NBINS; ++i)
+		if (lens[i] > max_count)
+			max_count = lens[i];
+
+	int hist_matrix[max_count][NBINS];
+	for (i = 0; i < max_count; ++i)
+		for (j = 0; j < NBINS; ++j)
+			hist_matrix[i][j] = 0;
+	
+	for (i = 0; i < NBINS; ++i)
 		for (j = 0; j < lens[i]; ++j)
-			putchar('|');
+			hist_matrix[max_count-1-j][i] = 1;
+
+	/* Draw histogram */
+	for (i = 0; i < max_count; ++i) {
+		for (j = 0; j < NBINS; ++j)
+			if (hist_matrix[i][j])
+				printf("%4c", '#');
+			else
+				printf("%4c", ' ');
 		putchar('\n');
 	}
-
-	printf("+%d ", NBINS-1);
-	for (j = 0; j < lens[NBINS - 1]; ++j)
-		putchar('|');
 	putchar('\n');
+	for (i = 0; i < NBINS - 1; ++i)
+		printf("%4d", i + 1);
+	printf(" +%2d\n", NBINS - 1);
+	
 
 }
