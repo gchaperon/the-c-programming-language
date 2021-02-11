1993156dde8e69815e736ee6318f35cbad6cbee3
Gabriel Chaperon <gabrielchaperonb@gmail.com>
2021-02-11 20:42:46 -0300

    [PATCH] ppc64: Fix booting on latest G5 models
    The latest speedbumped Apple G5 models have a "bug" in the Open Firmware
    device tree that lacks the proper interrupt routing information for the
    northbridge i2c controller.  Apple's driver silently falls back into a
    sub-optimal "polled" mode (heh, maybe they didn't even notice the bug
    because of that :), our driver didn't properly check and crashes :(
    
    This patch fixes our driver to not crash, and adds code to the
    prom_init() OF trampoline code that detects the "bug" and adds the
    missing information back for this chipset revision.  This fixes booting
    and thermal control on these models.
    
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>
    
    Commit date 2005-05-23T00:34:42Z

---
+++ chapter4/4-04.c
@@ -0,0 +1,155 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAXOP 100
+#define NUMBER '0'
+
+int getop(char []);
+void push(double);
+double pop(void);
+double peek(void);
+void swap(void);
+void clear(void);
+
+main()
+{
+	int type;
+	double op2;
+	char s[MAXOP];
+
+	while ((type = getop(s)) != EOF) {
+		switch (type) {
+			case NUMBER:
+				push(atof(s));
+				break;
+			case '+':
+				push(pop() + pop());
+				break;
+			case '*':
+				push(pop() * pop());
+				break;
+			case '-':
+				op2 = pop();
+				push(pop() - op2);
+				break;
+			case '/':
+				op2 = pop();
+				if (op2 != 0)
+					push(pop() / op2);
+				else
+					printf("error: zero divisor\n");
+				break;
+			case 'p':
+				printf("\t%.8g\n", peek());
+				break;
+			case 'd':
+				push(peek());
+				break;
+			case 's':
+				swap();
+				break;
+			case 'c':
+				clear();
+				break;
+			case '\n':
+				printf("\t%.8g\n", pop());
+				break;
+			default:
+				printf("error: unknown command %s\n", s);
+				break;
+		}
+	}
+	return 0;
+}
+
+#define MAXVAL 100
+int sp = 0;
+double val[MAXVAL];
+
+void push(double f)
+{
+	if (sp < MAXVAL)
+		val[sp++] = f;
+	else
+		printf("error: stack full, can't push %g\n", f);
+}
+
+double pop(void)
+{
+	if (sp > 0)
+		return val[--sp];
+	else {
+		printf("error: stack empty\n");
+		return 0.0;
+	}
+}
+
+double peek(void)
+{
+	double x;
+	
+	x = pop();
+	push(x);
+	return x;
+}
+void swap(void)
+{
+	double x1, x2;
+	
+	x2 = pop();
+	x1 = pop();
+	push(x2);
+	push(x1);
+}
+
+void clear(void)
+{
+	sp = 0;
+}
+
+
+#include <ctype.h>
+
+int getch(void);
+void ungetch(int);
+
+int getop(char s[])
+{
+	int i, c;
+	
+	while ((s[0] = c = getch()) == ' ' || c == '\t')
+		;
+	s[1] = '\0';
+	if (!isdigit(c) && c != '.')
+		return c;
+	i = 0;
+	if (isdigit(c))
+		while (isdigit(s[++i] = c = getch()))
+			;
+	if (c == '.')
+		while (isdigit(s[++i] = c = getch()))
+			;
+	s[i] = '\0';
+	if (c != EOF)
+		ungetch(c);
+	return NUMBER;
+}
+
+
+#define BUFSIZE 100
+char buf[BUFSIZE];
+int bufp = 0;
+
+int getch(void)
+{
+	return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+void ungetch(int c)
+{
+	if (bufp < BUFSIZE)
+		buf[bufp++] = c;
+	else
+		printf("ungetch: too many characters\n");
+}
+
